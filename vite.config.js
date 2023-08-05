cdd8fb7c3a995adfd3e247e43d443e23e7518abe
= <=>
2023-08-04 20:18:05 -0500

    add dotenv

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import { fileURLToPath } from 'url';
+import 'dotenv/config';
 
-const env = 'dev'; // or process.env.NODE_ENV etc.
+const env = process.env.myEnv;
 
 const apiPath = env === 'production' ? 'api' : 'api/mocks';
 const fullApiPath = fileURLToPath(new URL(apiPath, import.meta.url));
